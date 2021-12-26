064897cf05123b1a3f19a394200021fd22cf99e4
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-12-26 22:08:13 +0200

    feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,21 @@ import { PORT } from './common/config';
 import app from './app';
 import logger from './middlewares/logger';
 
+process.on('uncaughtException', (err, origin) => {
+  logger.error(err.message, origin);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  logger.error(`Unhandled Rejection, reason: ${reason}`);
+  setTimeout(() => {
+    process.exit(1);
+  }, 500);
+});
+
+// Promise.reject(Error('Oops!'));
+// throw Error('Oops!');
+
 /**
  * Starts server
  * @param PORT - port for app
