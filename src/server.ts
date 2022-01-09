d68833ca61e568a123b38762cfe27064edc900d5
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2022-01-09 16:07:41 +0200

    fix: add postgress variables

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ errorHandler(app);
  */
 const start = async () => {
   try {
-    await app.listen(PORT);
+    await app.listen(PORT, '0.0.0.0');
     console.log(`Server is running on port ${PORT}`)
   } catch (err) {
     logger.error(err);
