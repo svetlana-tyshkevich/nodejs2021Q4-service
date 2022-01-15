f02c797c73f540c092a624c8badce813f129c85f
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2022-01-16 00:08:00 +0200

    refactor: replace dbconfig to the separate file

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,14 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import {
-  PORT,
-  POSTGRES_PORT,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  POSTGRES_DB,
-  POSTGRES_HOST,
-} from './common/config';
+import { PORT } from './common/config';
 import app from './app';
 import logger from './middlewares/logger';
 import errorHandler from './middlewares/errorHandler';
-
+import dbConfig from './common/ormconfig'
 
 errorHandler(app);
 
-createConnection({
-  type: 'postgres',
-  host: POSTGRES_HOST,
-  port: POSTGRES_PORT,
-  username: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  database: POSTGRES_DB,
-  entities: ['src/entity/**/*{.ts,.js}'],
-  synchronize: true,
-  logging: false,
-})
+createConnection(dbConfig)
   .then(async (connection) => {
     if (connection.isConnected) {
       console.log('DataBase is connected');
@@ -40,14 +23,3 @@ createConnection({
     logger.error(err);
     process.exit(1);
   });
-
-// const start = async () => {
-//   try {
-//     await app.listen(PORT, '0.0.0.0');
-//     console.log(`Server is running on port ${PORT}`);
-//   } catch (err) {
-//     logger.error(err);
-//     process.exit(1);
-//   }
-// };
-// start();
